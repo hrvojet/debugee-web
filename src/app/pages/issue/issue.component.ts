e4ed998ce1501d4044fec5971e983c0312db39bc
Hrvoje <hrvojetrcak@gmail.com>
2023-05-01 01:53:58 +0200

    Add fetching for issue screen

--- src/app/pages/issue/issue.component.ts
+++ src/app/pages/issue/issue.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IssueService } from '../../services/issue/issue.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-issue',
-  templateUrl: './issue.component.html',
-  styleUrls: ['./issue.component.css']
+	selector: 'app-issue',
+	templateUrl: './issue.component.html',
+	styleUrls: ['./issue.component.css'],
 })
 export class IssueComponent implements OnInit {
+	ISSUES: any;
+	@Input() issues: any;
 
-  constructor() { }
+	constructor(private issueService: IssueService, private route: ActivatedRoute) {
+		// @ts-ignore
+		// this.id = this.router.getCurrentNavigation()?.extras.state['id'] as number;
+	}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.getIssues();
+		console.log(this.issues);
+	}
 
+	getIssues(): void {
+		const id = this.route.snapshot.paramMap.get('projectId');
+
+		this.issueService.getIssues(Number(id)).subscribe((data) => {
+			console.log('data: ' + data);
+			this.ISSUES = data;
+		});
+	}
 }
