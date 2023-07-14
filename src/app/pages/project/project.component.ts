d493b0e9703fea4d142b9081f2c2c5bfcadd9f89
Hrvoje <hrvojetrcak@gmail.com>
2023-07-14 19:00:41 +0200

    Change GET query for projects to return page instead of list

--- src/app/pages/project/project.component.ts
+++ src/app/pages/project/project.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { ProjectService } from '../../shared/services/project.service';
 import { IProject } from '../../shared/models/project.model';
+import { MatTableDataSource } from '@angular/material/table';
+import { IIssue } from '../../shared/models/issue.model';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { map, merge, startWith, switchMap } from 'rxjs';
 
 @Component({
 	selector: 'app-project',
@@ -8,14 +13,44 @@ import { IProject } from '../../shared/models/project.model';
 	styleUrls: ['./project.component.css'],
 	encapsulation: ViewEncapsulation.None,
 })
-export class ProjectComponent implements OnInit {
+export class ProjectComponent implements OnInit, AfterViewInit {
 	PROJECTS?: IProject[];
+
+	resultsLength = 0;
+	displayedColumns: string[] = ['title'];
+
+	dataSource = new MatTableDataSource<IProject>();
+	@ViewChild(MatPaginator) paginator!: MatPaginator;
+	@ViewChild(MatSort) sort!: MatSort;
 
 	constructor(private projectService: ProjectService) {}
 
 	ngOnInit(): void {
-		this.projectService.getProjects().subscribe((data) => {
-			this.PROJECTS = data;
-		});
+		this.dataSource.paginator = this.paginator;
+		this.dataSource.sort = this.sort;
+	}
+
+	ngAfterViewInit(): void {
+		debugger;
+		this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
+
+		merge(this.sort.sortChange, this.paginator.page)
+			.pipe(
+				startWith({}),
+				switchMap(() => {
+					return this.projectService.getProjects();
+				}),
+				map((data) => {
+					if (data === null) {
+						return [];
+					}
+
+					this.resultsLength = data.totalElements;
+					return data.content;
+				})
+			)
+			.subscribe((data) => {
+				this.dataSource.data = data;
+			});
 	}
 }
--- src/app/shared/services/project.service.ts
+++ src/app/shared/services/project.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { IProject } from '../models/project.model';
+import { IPage } from '../models/page/page.model';
 
 @Injectable({
 	providedIn: 'root',
@@ -10,7 +11,7 @@ export class ProjectService {
 	constructor(private http: HttpClient) {}
 
 	getProjects() {
-		return this.http.get<IProject[]>(environment.protocol + environment.debugeeDomain + '/projects');
+		return this.http.get<IPage<IProject>>(environment.protocol + environment.debugeeDomain + '/projects');
 	}
 
 	getProjectById(id: number) {
