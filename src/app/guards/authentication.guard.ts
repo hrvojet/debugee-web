40e92f9208dc89a0aa41e457f104679d2d9575c5
Hrvoje <hrvojetrcak@gmail.com>
2023-09-28 18:41:18 +0200

    Remove console.log from AuthGuard

--- src/app/guards/authentication.guard.ts
+++ src/app/guards/authentication.guard.ts
@@ -13,7 +13,6 @@ export class AuthenticationGuard implements CanActivate {
 		route: ActivatedRouteSnapshot,
 		state: RouterStateSnapshot
 	): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-		console.log('Oh no kiddo!');
 		return this.session.isUserLoggedIn();
 	}
 }
