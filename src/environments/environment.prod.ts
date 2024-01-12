c1dc7933d5ecc85024d2c89ca6f1c5a9bff6310e
Hrvoje <hrvojetrcak@gmail.com>
2024-01-12 19:56:18 +0100

    Add prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
 	production: true,
+	clientId: '4aaa243bf068dd1c42616d15d555e394d9a76cc51ca9d2fac4af89ce5e035a10',
+	protocol: 'http://',
+	debugeeDomain: 'localhost:8080',
+	debugeeTokenApi: '/callback/gitlab',
+	gitLabDomain: '164.90.165.32',
+	gitLabTokenApi: '/oauth/authorize',
 };
