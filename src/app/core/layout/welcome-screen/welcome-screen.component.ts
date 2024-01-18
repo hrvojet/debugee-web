2e38bffe3ba84b4ff7887e076200e02572941373
Hrvoje <hrvojetrcak@gmail.com>
2024-01-19 00:34:20 +0100

    Implement landing page

--- src/app/core/layout/welcome-screen/welcome-screen.component.ts
+++ src/app/core/layout/welcome-screen/welcome-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SessionService } from '../../../services/auth/session.service';
 
 @Component({
 	selector: 'app-welcome-screen',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./welcome-screen.component.css'],
 })
 export class WelcomeScreenComponent implements OnInit {
-	constructor() {}
+	constructor(private sessionService: SessionService) {}
 
 	ngOnInit(): void {}
+
+	login() {
+		this.sessionService.login();
+	}
 }
