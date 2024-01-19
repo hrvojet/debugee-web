48b2a1a81ffaacfae3466f055bfb0a7ddd46a2b3
Hrvoje <hrvojetrcak@gmail.com>
2024-01-19 19:44:13 +0100

    Move script to dir, create deployment script

--- deployment-scripts/deploy-artifact-to-server.sh
+++ deployment-scripts/deploy-artifact-to-server.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-### Publish script for debugee-web
+### Publish script for debugee-web to deploy .tar.gz to server
 # BEFORE USE: dependencies required: jq
-#
 #
 ###
 
@@ -25,13 +24,14 @@ tar -czvf web-debugee.tar.gz debugee-web || echo "Failed to tar"
 ls
 
 #scp
-scp web-debugee.tar.gz hrvoje@20.82.136.123:~ || echo "Failed to scp"
+scp web-debugee.tar.gz hrvoje@20.82.136.123:~/.web-deploy || echo "Failed to scp"
 
 #run remote deployment script
 # TODO: nohup java -jar -Dspring.profiles.active=prod backend-0.0.1-SNAPSHOT.jar > be.log 2>&1 &
+ssh hrvoje@20.82.136.123 "/home/hrvoje/web-deploy.sh"
 
 
 #delete local tar
 rm web-debugee.tar.gz
-cd "$ROOT_WORK_DIR" || echo "This really shouldn't have failed..."
+cd "$ROOT_WORK_DIR" || echo "This really should not have failed..."
 pwd
