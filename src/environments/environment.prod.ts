29d1550442a0a5e49ecbc6217e9ab278eb0f8362
Hrvoje <hrvojetrcak@gmail.com>
2024-01-29 21:36:57 +0100

    Change debugee domain in env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
 	production: true,
 	clientId: '4aaa243bf068dd1c42616d15d555e394d9a76cc51ca9d2fac4af89ce5e035a10',
 	protocol: 'http://',
-	debugeeDomain: '20.82.136.123',
+	debugeeDomain: 'www.debugee.live',
 	redirectUri: '20.82.136.123/callback/gitlab',
 	gitLabDomain: '164.90.165.32',
 	gitLabTokenApi: '/oauth/authorize',
